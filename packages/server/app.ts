8b2ea249d900c526889b483980bb87cbf0ac7cf2
Kael <piskael03@gmail.com>
2019-01-14 16:28:49 -0500

    test pre-commit

--- packages/server/app.ts
+++ packages/server/app.ts
@@ -1,4 +1,4 @@
-import "reflect-metadata";
+import 'reflect-metadata';
 import * as express from 'express';
 import { UserResolver } from './graphql/resolvers/userResolver';
 import { ApolloServer } from 'apollo-server-express';
