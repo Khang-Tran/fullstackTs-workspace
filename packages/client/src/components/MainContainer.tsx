5cc782c669e0718255f28596d9e28950135282e7
Kael <piskael03@gmail.com>
2019-01-23 14:29:00 -0500

    add client structure

---
+++ packages/client/src/components/MainContainer.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import GlobalStyles from '../styles/globalStyles';
+
+const MainContainer = () => (
+	<Fragment>
+		<GlobalStyles/>
+		<div>App</div>
+	</Fragment>
+);
+
+export default MainContainer;
--- packages/client/src/queries/userQueries.ts
+++
@@ -1,9 +0,0 @@
-import gql from 'graphql-tag';
-
-export const GET_USER_BY_EMAIL = gql`
-	query {
-		user(email: "kael@gmail.com") {
-			lastName
-		}
-	}
-`;
